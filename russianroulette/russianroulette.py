c6d89eafb753fb4c32fa745880408a1d434dbcfc
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-03-15 11:56:53 +0800

    Reformatting

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -3,6 +3,7 @@ import random
 import asyncio
 import discord
 
+
 class RussianRoulette(commands.Cog):
     """russian roulette tbh"""
 
@@ -12,10 +13,22 @@ class RussianRoulette(commands.Cog):
     @commands.command()
     async def russianroulette(self, ctx):
         """try your luck"""
-        responses_list=["You\'re safe!","You\'re safe!","You\'re safe!","You\'re safe!","You\'re safe!","BANG! You're dead now."]
-        choice=random.choice(responses_list)
-        embed=discord.Embed(description="You pulled the trigger and...", color=await ctx.embed_color())
-        msg=await ctx.send(embed=embed)
+        responses_list = [
+            "You're safe!",
+            "You're safe!",
+            "You're safe!",
+            "You're safe!",
+            "You're safe!",
+            "BANG! You're dead now.",
+        ]
+        choice = random.choice(responses_list)
+        embed = discord.Embed(
+            description="You pulled the trigger and...", color=await ctx.embed_color()
+        )
+        msg = await ctx.send(embed=embed)
         await asyncio.sleep(2)
-        embed2=discord.Embed(description=f"You pulled the trigger and...\n\n{choice}", color=await ctx.embed_color())
+        embed2 = discord.Embed(
+            description=f"You pulled the trigger and...\n\n{choice}",
+            color=await ctx.embed_color(),
+        )
         await msg.edit(embed=embed2)
