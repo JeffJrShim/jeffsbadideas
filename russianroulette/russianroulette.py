2e55c3a7879df50e951eff95b2e124b22b2945f0
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-03-15 09:48:01 +0800

    Will this work lmao

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -1,5 +1,6 @@
 from redbot.core import commands
 import random
+import asyncio
 
 class RussianRoulette(commands.Cog):
     """russian roulette tbh"""
@@ -10,4 +11,10 @@ class RussianRoulette(commands.Cog):
     @commands.command()
     async def russianroulette(self, ctx):
         """try your luck"""
-        await ctx.send("testing if this works")
+        responses_list=["You\'re safe!","You\'re safe!","You\'re safe!","You\'re safe!","You\'re safe!","BANG! You're dead now."]
+        choice=random.choice(responses_list)
+        embed=discord.Embed(description="You pulled the trigger and...", color=await ctx.embed_color())
+        msg=await ctx.send(embed=embed)
+        await asyncio.sleep(2)
+        embed2=discord.Embed(description=f"You pulled the trigger and...\n\n{choice}", color=await ctx.embed_color())
+        await msg.edit(embed=embed2)
