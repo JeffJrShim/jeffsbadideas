fc0c540068a12d2211b63b2d7a6c0fdf78519ded
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-03-19 23:17:08 +0800

    Fix and rewrite this cog slightly

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -1,4 +1,4 @@
-from redbot.core import commands
+from redbot.core import commands, Config
 import random
 import asyncio
 import discord
@@ -9,26 +9,46 @@ class RussianRoulette(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=723841)
+        default_guild = {
+            chances = "6"
+        }
+        self.config.register_guild(**default_guild)
 
     @commands.command()
     async def russianroulette(self, ctx):
         """try your luck"""
-        responses_list = [
-            "You're safe!",
-            "You're safe!",
-            "You're safe!",
-            "You're safe!",
-            "You're safe!",
-            "BANG! You're dead now.",
-        ]
-        choice = random.choice(responses_list)
+        russianroulettegenerator=random.randint(1,chances)
         embed = discord.Embed(
             description="You pulled the trigger and...", color=await ctx.embed_color()
-        )
+            )
         msg = await ctx.send(embed=embed)
-        await asyncio.sleep(2)
-        embed2 = discord.Embed(
-            description=f"You pulled the trigger and...\n\n{choice}",
-            color=await ctx.embed_color(),
-        )
-        await msg.edit(embed=embed2)
+        if russianroulettegenerator == 3:
+            embed2 = discord.Embed(
+                description=f"You pulled the trigger and...\n\nBANG! You're dead!",
+                color=await ctx.embed_color(),
+            )
+            await msg.edit(embed=embed2)
+        else: 
+            embed3 = discord.Embed(
+                description=f"You pulled the trigger and...\n\nBANG! You're safe!",
+                color=await ctx.embed_color(),
+            )
+            await msg.edit(embed=embed3)
+    
+    @commands.mod()
+    @commands.group()
+    async def russianrouletteset(self, ctx):
+        """Set defaults for the russian roulette"""
+        pass
+    
+    @commands.mod()
+    @russianrouletteset.command()
+    async def setdefault(self, ctx, new_value: int = 6):
+        """Sets the chamber value for the Russian Roulette."""
+        if new_value < 6:
+            return await ctx.send("The new value cannot be less than 6! Sorry D:")
+        else:
+            await self.config.guild(ctx.guild).new_value.set(new_value)
+            await ctx.send("The new value has been set.")
+        
